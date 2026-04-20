d2bb25855c2740d8e3d544fc5691f94ccfe034db
Vlad Zotov <hramzik.ru@yandex.ru>
2026-04-20 06:04:19 +0300

    Add task B review changes

--- Contest/b/B.kt
+++ Contest/b/B.kt
@@ -1,14 +1,23 @@
 // Submission id 157505941
 class ModuledMatrix2x2(a00: Long, a01: Long, a10: Long, a11: Long, val module: Long)
 {
-    class ModuledLong(value: Long, var module: Long) {
-        var value = value % module
-        operator fun plus(other: ModuledLong) = ModuledLong((value + other.value) % module, module)
-        operator fun times(other: ModuledLong) = ModuledLong((value * other.value) % module, module)
-    }
+    class ModuledLong(value: Long, val module: Long) {
+        val value: Long = ((value % module) + module) % module
 
-    constructor(_a00: ModuledLong, _a01: ModuledLong, _a10: ModuledLong, _a11: ModuledLong):
-            this(_a00.value, _a01.value, _a10.value, _a11.value, _a00.module)
+        private fun checkModulesMatching(other: ModuledLong) {
+            require(module == other.module) { "Modules must match" }
+        }
+
+        operator fun plus(other: ModuledLong): ModuledLong {
+            checkModulesMatching(other)
+            return ModuledLong((value + other.value) % module, module)
+        }
+
+        operator fun times(other: ModuledLong): ModuledLong {
+            checkModulesMatching(other)
+            return ModuledLong((value * other.value) % module, module)
+        }
+    }
 
     val _a00 = ModuledLong(a00, module)
     val _a01 = ModuledLong(a01, module)
@@ -42,7 +51,7 @@ class ModuledMatrix2x2(a00: Long, a01: Long, a10: Long, a11: Long, val module: L
         val result10 = _a10 * other._a00 + _a11 * other._a10
         val result11 = _a10 * other._a01 + _a11 * other._a11
         
-        return ModuledMatrix2x2(result00, result01, result10, result11)
+        return ModuledMatrix2x2(result00.value, result01.value, result10.value, result11.value, module)
     }
 }
 
