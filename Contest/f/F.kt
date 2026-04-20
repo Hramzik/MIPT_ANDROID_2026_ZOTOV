762fe47fe2657b846f15eeddccad2fa8775b65cf
Vlad Zotov <hramzik.ru@yandex.ru>
2026-04-20 05:56:13 +0300

    Add task F review changes

--- Contest/f/F.kt
+++ Contest/f/F.kt
@@ -11,6 +11,15 @@ data class Animal(val id: Int, val type: Animal.Type, val height: Int, var keepe
     }
 }
 
+fun printAnimalIds(animals: SortedSet<Animal>?) {
+    if (animals.isNullOrEmpty()) {
+        println("NONE")
+    }
+    else {
+        println(animals.joinToString(" ") { it.id.toString() })
+    }
+}
+
 data class Keeper(val id: Int, val name: String)
 
 class ZooDatabase {
@@ -24,77 +33,38 @@ class ZooDatabase {
     private val keeperIdToAnimals = mutableMapOf<Int, TreeSet<Animal>>()
     private val keeperNameToAnimals = mutableMapOf<String, TreeSet<Animal>>()
 
-    fun onQuery(line: String) {
-        val words = line.split(' ')
-        when (words[0]) {
-            "ANIMAL_ADD" -> {
-                val id = words[1].toInt()
-                val type = Animal.Type.valueOf(words[2])
-                val height = words[3].toInt()
-                val animal = Animal(id, type, height, null)
-                idToAnimal[id] = animal
-                animals.add(animal)
-                typeToAnimals.getOrPut(type) { TreeSet(animalComparator) }.add(animal)
-                if (type != Animal.Type.FISH) talkingAnimals.add(animal)
-            }
-            "ANIMAL_DEL" -> {
-                val animalId = words[1].toInt()
-                unassignAnimal(animalId)
-
-                val animal = idToAnimal.remove(animalId) ?: return
-                animals.remove(animal)
-                typeToAnimals[animal.type]?.remove(animal)
-                if (animal.type != Animal.Type.FISH) talkingAnimals.remove(animal)
-            }
-            "KEEPER_ADD" -> {
-                val id = words[1].toInt()
-                val name = words[2]
-                idToKeeper[id] = Keeper(id, name)
-            }
-            "ASSIGN" -> {
-                val animalId = words[1].toInt()
-                val keeperId = words[2].toInt()
-                val animal = idToAnimal[animalId] ?: return
-                val keeper = idToKeeper[keeperId] ?: return
-
-                if (animal.keeperId == keeperId) return
-                unassignAnimal(animalId)
-                keeperIdToAnimals.getOrPut(keeperId) { TreeSet(animalComparator) }.add(animal)
-                keeperNameToAnimals.getOrPut(keeper.name) { TreeSet(animalComparator) }.add(animal)
-                animal.keeperId = keeperId
-            }
-            "UNASSIGN" -> {
-                val animalId = words[1].toInt()
-                unassignAnimal(animalId)
-            }
-            "Q_KEEPER" -> {
-                val keeperId = words[1].toInt()
-                printAnimalIds(keeperIdToAnimals[keeperId])
-            }
-            "Q_NAME" -> {
-                val name = words[1]
-                printAnimalIds(keeperNameToAnimals[name])
-            }
-            "Q_TALL" -> {
-                val h = words[1].toInt()
-                printAnimalIds(animals.tailSet(Animal(Int.MIN_VALUE, Animal.Type.CAT, h + 1, null)))
-            }
-            "Q_SOUND" -> {
-                printAnimalIds(talkingAnimals)
-            }
-            "Q_TYPE" -> {
-                val type = Animal.Type.valueOf(words[1])
-                printAnimalIds(typeToAnimals[type])
-            }
-        }
+    fun addAnimal(id: Int, type: Animal.Type, height: Int) {
+        val animal = Animal(id, type, height, null)
+        idToAnimal[id] = animal
+        animals.add(animal)
+        typeToAnimals.getOrPut(type) { TreeSet(animalComparator) }.add(animal)
+        if (type != Animal.Type.FISH) talkingAnimals.add(animal)
     }
 
-    private fun printAnimalIds(animals: SortedSet<Animal>?) {
-        if (animals.isNullOrEmpty()) println("NONE")
-        else println(animals.joinToString(" ") { it.id.toString() })
+    fun deleteAnimal(animalId: Int) {
+        unassignAnimal(animalId)
+        val animal = idToAnimal.remove(animalId) ?: return
+        animals.remove(animal)
+        typeToAnimals[animal.type]?.remove(animal)
+        if (animal.type != Animal.Type.FISH) talkingAnimals.remove(animal)
     }
 
-    private fun unassignAnimal(animalId: Int) {
+    fun addKeeper(id: Int, name: String) {
+        idToKeeper[id] = Keeper(id, name)
+    }
+
+    fun assignAnimalToKeeper(animalId: Int, keeperId: Int) {
+        val animal = idToAnimal[animalId] ?: return
+        val keeper = idToKeeper[keeperId] ?: return
+
+        if (animal.keeperId == keeperId) return
+        unassignAnimal(animalId)
+        keeperIdToAnimals.getOrPut(keeperId) { TreeSet(animalComparator) }.add(animal)
+        keeperNameToAnimals.getOrPut(keeper.name) { TreeSet(animalComparator) }.add(animal)
+        animal.keeperId = keeperId
+    }
+
+    fun unassignAnimal(animalId: Int) {
         val animal = idToAnimal[animalId] ?: return
         animal.keeperId?.let { keeperId ->
             keeperIdToAnimals[keeperId]?.remove(animal)
@@ -103,6 +73,16 @@ class ZooDatabase {
 
         animal.keeperId = null
     }
+
+    fun getAnimalsByKeeperId(keeperId: Int): SortedSet<Animal> = keeperIdToAnimals[keeperId] ?: TreeSet(animalComparator)
+
+    fun getAnimalsByKeeperName(name: String): SortedSet<Animal> = keeperNameToAnimals[name] ?: TreeSet(animalComparator)
+
+    fun getTallAnimals(minHeightExclusive: Int): SortedSet<Animal> = animals.tailSet(Animal(Int.MIN_VALUE, Animal.Type.CAT, minHeightExclusive + 1, null))
+
+    fun getTalkingAnimals(): SortedSet<Animal> = talkingAnimals
+
+    fun getAnimalsByType(type: Animal.Type): SortedSet<Animal> = typeToAnimals[type] ?: TreeSet(animalComparator)
 }
 
 fun main() {
@@ -110,8 +90,54 @@ fun main() {
     val q = scanner.nextInt()
     scanner.nextLine()
     val zooDatabase = ZooDatabase()
+
     repeat(q) {
         val line = scanner.nextLine()
-        zooDatabase.onQuery(line)
+        val words = line.split(' ')
+        when (words[0]) {
+            "ANIMAL_ADD" -> {
+                val id = words[1].toInt()
+                val type = Animal.Type.valueOf(words[2])
+                val height = words[3].toInt()
+                zooDatabase.addAnimal(id, type, height)
+            }
+            "ANIMAL_DEL" -> {
+                val animalId = words[1].toInt()
+                zooDatabase.deleteAnimal(animalId)
+            }
+            "KEEPER_ADD" -> {
+                val id = words[1].toInt()
+                val name = words[2]
+                zooDatabase.addKeeper(id, name)
+            }
+            "ASSIGN" -> {
+                val animalId = words[1].toInt()
+                val keeperId = words[2].toInt()
+                zooDatabase.assignAnimalToKeeper(animalId, keeperId)
+            }
+            "UNASSIGN" -> {
+                val animalId = words[1].toInt()
+                zooDatabase.unassignAnimal(animalId)
+            }
+            "Q_KEEPER" -> {
+                val keeperId = words[1].toInt()
+                printAnimalIds(zooDatabase.getAnimalsByKeeperId(keeperId))
+            }
+            "Q_NAME" -> {
+                val name = words[1]
+                printAnimalIds(zooDatabase.getAnimalsByKeeperName(name))
+            }
+            "Q_TALL" -> {
+                val h = words[1].toInt()
+                printAnimalIds(zooDatabase.getTallAnimals(h))
+            }
+            "Q_SOUND" -> {
+                printAnimalIds(zooDatabase.getTalkingAnimals())
+            }
+            "Q_TYPE" -> {
+                val type = Animal.Type.valueOf(words[1])
+                printAnimalIds(zooDatabase.getAnimalsByType(type))
+            }
+        }
     }
 }
