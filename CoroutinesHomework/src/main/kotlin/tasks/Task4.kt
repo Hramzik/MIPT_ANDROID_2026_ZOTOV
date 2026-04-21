340341ea883cfe3d267b68bc850a73a6af26e0ef
Vlad Zotov <hramzik.ru@yandex.ru>
2026-04-21 08:50:52 +0300

    Do task 4

--- CoroutinesHomework/src/main/kotlin/tasks/Task4.kt
+++ CoroutinesHomework/src/main/kotlin/tasks/Task4.kt
@@ -1,5 +1,9 @@
 package tasks
 
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
 /**
  * Задание 4: Дедуплицирующий загрузчик (Deduplicating Loader)
  *
@@ -23,7 +27,52 @@ interface DeduplicatingLoader<K, V> {
 
 // TODO: реализуйте этот класс
 class DeduplicatingLoaderImpl<K, V> : DeduplicatingLoader<K, V> {
+    private val keyToDeferred = mutableMapOf<K, CompletableDeferred<V>>()
+    private val keyToDeferredMutex = Mutex()
+
     override suspend fun load(key: K, loader: suspend (K) -> V): V {
-        TODO("Not yet implemented")
+        val hasDeferredAlreadyExisted: Boolean = keyToDeferredMutex.withLock {
+            keyToDeferred.containsKey(key).also {
+                if (!it) {
+                    createDeferred(key)
+                }
+            }
+        }
+
+        if (hasDeferredAlreadyExisted) {
+            return waitForExistingDeferred(key)
+        }
+        return completeDeferred(key, loader)
+
+    }
+
+    private suspend fun waitForExistingDeferred(key: K): V {
+        val deferred = keyToDeferredMutex.withLock {
+            keyToDeferred[key]!!
+        }
+        return deferred.await()
+    }
+
+    private suspend fun createDeferred(key: K) {
+        keyToDeferred[key] = CompletableDeferred()
+    }
+
+    private suspend fun completeDeferred(key: K, loader: suspend (K) -> V): V {
+        val deferred = keyToDeferred[key]!!
+
+        try {
+            val deferredResult = loader(key)
+            deferred.complete(deferredResult)
+            return deferredResult
+        }
+        catch (e: Throwable) {
+            deferred.completeExceptionally(e)
+            throw e
+        }
+        finally {
+            keyToDeferredMutex.withLock {
+                keyToDeferred.remove(key)
+            }
+        }
     }
 }
